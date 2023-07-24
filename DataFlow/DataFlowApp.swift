2e154b9450f58689344704ee35575ea8a87b33ba
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-07-24 20:03:30 +0300

    Initial Commit

---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Vasichko Anna on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
